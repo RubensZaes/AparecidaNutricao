204106de0e8067d7ebf8740784082bc393b3128f
RubensZaes <rubenszaes@gmail.com>
2021-03-02 11:01:07 -0300

    Resposta de Requisição

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -4,13 +4,24 @@ botaoAddPacientes.addEventListener('click', function(){
     console.log('Buscando dados...');
     let xhr = new XMLHttpRequest()
     xhr.open('GET', 'https://api-pacientes.herokuapp.com/pacientes')
-    xhr.addEventListener('load', function(){
-        let resposta = xhr.responseText
-        let pacientes = JSON.parse(resposta)        
 
-        pacientes.forEach(function(paciente){
-            adicionaPacienteNaTabela(paciente)
-        })
+    xhr.addEventListener('load', function(){
+
+        let erroAjax = document.querySelector('#erro-ajax')
+
+        if(xhr.status == 200){
+            erroAjax.classList.add('invisivel')
+            let resposta = xhr.responseText
+            let pacientes = JSON.parse(resposta)        
+
+            pacientes.forEach(function(paciente){
+                adicionaPacienteNaTabela(paciente)
+            })
+        } else {
+            erroAjax.classList.remove('invisivel')
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+        }
     })
     xhr.send()
 })
